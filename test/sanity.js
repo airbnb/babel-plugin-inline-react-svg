e5e0f699469959c100b593ea9ceb2c0cf855a2e3
Ryan Pfister <interpfister@gmail.com>
2017-08-23 16:08:48 -0400

    Implement case-sensitivity check

--- test/sanity.js
+++ test/sanity.js
@@ -9,3 +9,19 @@ transformFile('test/fixtures/test.jsx', {
   if (err) throw err;
   console.log(result.code);
 });
+
+transformFile('test/fixtures/test-case-sensitive.jsx', {
+  presets: ['airbnb'],
+  plugins: [
+    ['../../src/index',
+    {
+        "caseSensitive": true
+    }]
+  ],
+}, (err, result) => {
+  if (err && err.message.indexOf('match case') !== -1) {
+    console.log("Test passed: Expected case sensitive error was thrown");
+  } else {
+    throw new Error("Test failed: Expected case sensitive error wasn't thrown");
+  }
+});
